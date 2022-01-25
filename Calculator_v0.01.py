03339322f80a9f3208efaa5f0c3a6e7c2ea764a6
Ayon Biswas <ayon.biswas95@gmail.com>
2022-01-25 17:14:41 +0530

    Basic Calculator-Non Dynamic

---
+++ Calculator_v0.01.py
@@ -0,0 +1,17 @@
+#non-dynamic calculator- Goal 1
+
+print()
+print("Calculator v1.01")
+print()
+Addition = 2+3
+print ("The addition of 2 and 3 is",Addition,".")
+print()
+Subtraction = 100-35
+print ("The subtraction of 100 and 35 is",Subtraction,".")
+print()
+Multiplication = 2*3
+print ("The multiplicative result of 2 and 3 is",Multiplication,".")
+print()
+Division = 18/3
+print ("The Division of 18 and 3 gives,",int(Division),".")
+print()
